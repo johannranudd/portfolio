99f0982edbe986e391e8271714e7fb75224db4fc
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-06-03 17:07:31 +0200

    scroll okay on all viewports

--- js/index.js
+++ js/index.js
@@ -189,7 +189,7 @@ links.forEach((link) => {
 
       if (window.innerWidth > 340) {
         window.scroll({
-          top: elemPosition - window.pageYOffset - elemOffset / 2.5,
+          top: elemPosition - window.pageYOffset - elemOffset / 2.55,
         });
       }
       if (window.innerWidth > 400) {
